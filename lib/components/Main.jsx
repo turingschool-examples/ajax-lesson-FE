c5ea7c2674bef83a30b23f6ce1c065a6b9913c70
Leta Keane <letakeane+turing@gmail.com>
2017-11-02 11:28:16 -0600

    Fetch data, set in state

--- lib/components/Main.jsx
+++ lib/components/Main.jsx
@@ -12,13 +12,24 @@ export default class Main extends Component {
     }
   }
 
+  componentDidMount() {
+    this.getTrivia();
+  }
 
   setQty(qty) {
     //add some codes
   }
 
   getTrivia() {
-    //add some codes
+    // fetch the url https://opentdb.com/api.php?amount=10&category=27&type=multiple
+    // then parse it (because it's a string right now)
+    // then console.log it
+    // don't forget a catch
+
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple')
+    .then( (response) => response.json() )
+    .then( (data) => this.setState({ triviaArray: data.results }) )
+    .catch( (error) => console.log('Error retreiving trivia: ', error) )
   }
 
   render() {
